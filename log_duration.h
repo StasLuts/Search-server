a093a59f897c33e6124a064b989240840e03a054
Stas Lyutsko <staslutzko@mail.ru>
2022-02-09 19:28:22 +0300

    add speed_test

--- log_duration.h
+++ log_duration.h
@@ -2,40 +2,33 @@
 
 #include <chrono>
 #include <iostream>
+#include <string>
 
-#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
-#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
-#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
-#define LOG_DURATION_STREAM(x, y) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+using namespace std;
+using namespace std::chrono;
 
-class LogDuration
-{
+class LogDuration {
 public:
-
-    using Clock = std::chrono::steady_clock;
-
-    LogDuration(const std::string& id)
-        : id_(id)
+    explicit LogDuration(const string& msg = "")
+        : message(msg + ": ")
+        , start(high_resolution_clock::now())
     {
     }
 
-    LogDuration(const std::string& id, std::ostream& os)
-        : id_(id), os_(os)
-    {
+    ~LogDuration() {
+        auto finish = high_resolution_clock::now();
+        auto dur = finish - start;
+        cerr << message
+            << duration_cast<microseconds>(dur).count()
+            << " microseconds" << endl;
     }
-
-    ~LogDuration()
-    {
-        using namespace std::chrono;
-        using namespace std::literals;
-
-        const auto end_time = Clock::now();
-        const auto dur = end_time - start_time_;
-        std::cerr << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
-    }
-
 private:
-    std::ostream& os_ = std::cerr;
-    const std::string id_;
-    const Clock::time_point start_time_ = Clock::now();
+    string message;
+    high_resolution_clock::time_point start;
 };
+
+#define UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
+#define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
+
+#define LOG_DURATION(message) \
+  LogDuration UNIQ_ID(__LINE__){message};
